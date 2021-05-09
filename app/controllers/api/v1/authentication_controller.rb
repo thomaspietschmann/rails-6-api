f9659a17a4a2b30c3e55c6fe00ae510edab8e14d
Thomas Pietschmann <thomas@pietschi.com>
2021-05-09 12:41:28 +0200

    last step authentication

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,14 +1,15 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
+      class AuthenticationError < StandardError; end
+
       skip_before_action :verify_authenticity_token
       rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      rescue_from AuthenticationError, with: :handle_unauthenticated
 
       def create
-        p params
-        p params.require(:password).inspect
+        raise AuthenticationError unless user.authenticate(params.require(:password))
 
-        user = User.find_by(username: params.require(:username))
         token = AuthenticationTokenService.call(user.id)
 
         render(
@@ -19,8 +20,16 @@ module Api
 
       private
 
+      def user
+        @user ||= User.find_by(username: params.require(:username)) # first call will run method, second call will just access the instance variable
+      end
+
       def parameter_missing(e)
         render json: { error: e.message }, status: :unprocessable_entity
+      end
+
+      def handle_unauthenticated
+        head :unauthorized
       end
     end
   end
